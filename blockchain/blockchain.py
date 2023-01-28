ce49064a5f8114289829104eb474e9719859f158
siddhardh-7 <cs20b1063@iiitdm.ac.in>
2023-01-28 20:16:10 +0530

    Update blockchain.py

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -89,5 +89,15 @@ def get_chain():
     }
     return jsonify(response) , 200
 
+#checking the valid of blockchain
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message' : 'Horray! Blockchain is valid'}
+    else:
+        response = {'message' : 'Blockchain is not valid!!'}
+    return jsonify(response) , 200
+
 # running the appilication
 app.run(host = '0.0.0.0' , port = 5000)
